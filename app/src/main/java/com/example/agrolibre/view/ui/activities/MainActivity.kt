a8a22c7a07a078503259305709944f5bf00875a4
Andres Chavez <andres.cz.chavez@gmail.com>
2021-11-20 18:35:16 -0500

    Agrego parte de la guía 12 con BotttomNavigationBar con fragments. (Pero no está navegando a otros fragments) también con SplashScreen y el Home junto con los diferentes fragments admin, order, commnents y el detalle para cada uno

--- app/src/main/java/com/example/agrolibre/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/agrolibre/view/ui/activities/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.agrolibre.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.agrolibre.R
+import com.example.agrolibre.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        configNav()
+
+    }
+    fun configNav(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragContent) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.btnvMenu).setupWithNavController(navController)
     }
 }
